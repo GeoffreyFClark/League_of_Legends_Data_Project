895e34cb54f0be06bc31063b9eeab7c7ca9b760d
GeoffreyFClark <97141856+GeoffreyFClark@users.noreply.github.com>
2023-03-07 10:21:25 -0700

    Update LeagueData_DiscordBot_Sanitized.py

--- LeagueData_DiscordBot_Sanitized.py
+++ LeagueData_DiscordBot_Sanitized.py
@@ -19,7 +19,7 @@ soup = BeautifulSoup(response.content, "html.parser")
 # Find the element(s) you want to scrape
 heading = soup.find("h1")
 
-# Use the text content of the element
+# Use the text content of the element(s)
 print(heading.text)
 
 
